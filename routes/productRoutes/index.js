81fd621e36bf78876c545a04e62ad4749e061828
aldrin mursidi <aldrn.dev@gmail.com>
2024-04-27 19:59:17 +0700

    feat : add models

--- routes/productRoutes/index.js
+++ routes/productRoutes/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const ProductController = require("../../controllers/productController");
 const router = express.Router();
 
-router.get("/show", ProductController.getProduct);
+router.get("/get", ProductController.getProduct);
 router.post("/add", ProductController.addProduct);
 
-router.get("/show/:id", ProductController.getProductDetail);
+router.get("/get/:id", ProductController.getProductDetail);
 router.put("/edit/:id", ProductController.editProduct);
 router.delete("/delete/:id", ProductController.deleteProduct);
 
